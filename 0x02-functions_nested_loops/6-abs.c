0b1458ca4a6db5e519db0c8b23dac5eda8402df3
Funkydude25 <topzyodumosu25@gmail.com>
2022-09-13 03:21:00 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs -> print an absolute integer
+ * @r: a number argument
+ * Return: absolute value of a number
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
