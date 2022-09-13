5b5e15afb392fed52d1b13deda6e90855353ed91
Funkydude25 <topzyodumosu25@gmail.com>
2022-09-13 04:22:39 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add -> two integers together
+ * @i: the first variable
+ * @j: the second variable
+ * Return: return the result
+ */
+
+int add(int i, int j)
+{
+	int n;
+	 n = i + j;
+	return (n);
+}
