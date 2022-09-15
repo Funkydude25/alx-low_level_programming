30cc8b6e93460415f87e5e7579b67fc310f86d92
Funkydude25 <topzyodumosu25@gmail.com>
2022-09-15 13:08:42 -0700

    Task 4a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,15 @@
 
 /**
  * print_most_numbers -> this prints most numbers
+ * do not print 2 and 4
+ * followed by a new line
+ * Return: Always 0
  */
 void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9', n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		if (n != '2' && n != '4')
 			_putchar(n);
